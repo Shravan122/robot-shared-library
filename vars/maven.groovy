287050d9d85a5e25d116ffd8a3244fa6e5fc6ba6
Shravan <shravan.kumbar@gmail.com>
2022-10-15 14:28:35 +0530

    Added

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,60 +1,77 @@
-def lintCheck() {
-    sh ''' 
-         echo Starting lint checks ${COMPONENT}
-         mvn checkstyle:check || true                        # lint checks
-         echo Lint Checks Completed for ${COMPONENT}
+def call() {
+    node {
+        git branch: 'main', url: "https://github.com/b50-clouddevops/${COMPONENT}.git"
+        env.APPTYPE="maven"
+        common.sonarCheck()
+        common.lintCheck()
+        sh 'mvn clean compile'
+        env.ARGS="-Dsonar.java.binaries=target/"
+        common.testCases()
+        if (env.TAG_NAME != null) {
+            common.artifact()
+        }
+    }
+} 
+
+
+
+//def lintCheck() {
+  //  sh ''' 
+    //     echo Starting lint checks ${COMPONENT}
+      //   mvn checkstyle:check || true                        # lint checks
+        // echo Lint Checks Completed for ${COMPONENT}
        
-       ''' 
-}
+       //''' 
+//}
 
 // def sonarCheck() {
    // sh ''' 
      //  sonar-scanner -Dsonar.host.url=http://172.31.0.180:9000 -Dsonar.java.binaries=target/  -Dsonar.projectKey=shipping -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}
      //''' 
 //}
-def call() {
-    pipeline {
-        agent any 
-    environment {
-        SONAR      = credentials('SONAR') 
-    }
-        stages {
-            stage('Lint Check') {
-                steps {
-                    script { 
-                        lintCheck()
-                    }
-                }
-            }
-             stage('Sonar Check') {
-                steps {
-                    script {  
-                        sh "mvn clean compile"
-                        env.ARGS="--Dsonar.java.binaries=target/"
-                         common.sonarCheck()
+//def call() {
+  //  pipeline {
+    //    agent any 
+    //environment {
+      //  SONAR      = credentials('SONAR') 
+    //}
+      //  stages {
+        //    stage('Lint Check') {
+          //      steps {
+            //        script { 
+              //          lintCheck()
+                //    }
+                //}
+            //}
+             //stage('Sonar Check') {
+               // steps {
+                 //   script {  
+                   //     sh "mvn clean compile"
+                     //   env.ARGS="--Dsonar.java.binaries=target/"
+                       //  common.sonarCheck()
                         
-                    }
-                }
-            } 
-            stage('Test Cases') {
-            parallel {
-                 stage('Unit Tests') {
-                     steps {
-                         sh 'echo Unit Test Cases Completed'
-                          }
-                     }
-                 stage('Integration Tests') {
-                     steps {
-                         sh 'echo Integration Test Cases Completed'
-                          }
-                     }
-                 stage('Functional Tests') {
-                     steps {
-                         sh 'echo Functional Test Cases Completed'
-                          }
-                     }
-                 }
-            }    // end of statges 
-         }
-     } 
-}
+                    //}
+                //}
+            //} 
+            //stage('Test Cases') {
+            //parallel {
+              //   stage('Unit Tests') {
+                //     steps {
+                  //       sh 'echo Unit Test Cases Completed'
+                    //      }
+                     //}
+                // stage('Integration Tests') {
+                  //  steps {
+                   //      sh 'echo Integration Test Cases Completed'
+                     //     }
+                    // }
+                // stage('Functional Tests') {
+                  //   steps {
+                    //     sh 'echo Functional Test Cases Completed'
+                      //    }
+                    // }
+                // }
+         //   }    // end of statges 
+       //  }
+    // } 
+//}
