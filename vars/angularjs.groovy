51a3e9041c8117bb6866a02cb5ced1c828d0c523
Shravan <shravan.kumbar@gmail.com>
2022-10-15 13:55:53 +0530

    Added

--- vars/angularjs.groovy
+++ vars/angularjs.groovy
@@ -1,53 +1,68 @@
-
-def lintCheck() {
-    sh ''' 
-         echo Starting lint checks ${COMPONENT}
-         echo Lint Checks Completed for ${COMPONENT}
-       
-       ''' 
-}
-
 def call() {
-    pipeline {
-        agent any 
-        stages {
-            stage('Lint Check') {
-                steps {
-                    script { 
-                        lintCheck()
-                    }
-                }
-            } 
-             stage('Sonar Check') {
-                steps {
-                    script {  
-                          env.ARGS="-Dsonar.sources=."
-                         common.sonarCheck()
+    node {
+        git branch: 'main', url: "https://github.com/b50-clouddevops/${COMPONENT}.git"
+        env.APPTYPE="angularjs"
+        common.sonarCheck()
+        common.lintCheck()
+        env.ARGS="-Dsonar.sources=."
+        common.testCases()
+        if (env.TAG_NAME != null) {
+            common.artifact()
+        }
+    }
+} 
+
+
+
+//def lintCheck() {
+ //   sh ''' 
+ //     #   echo Starting lint checks ${COMPONENT}
+  //    #   echo Lint Checks Completed for ${COMPONENT}
+       
+  //     ''' 
+//}
+
+// def call() {
+  //  pipeline {
+  //      agent any 
+    //    stages {
+    //        stage('Lint Check') {
+     //           steps {
+      //              script { 
+      //                  lintCheck()
+      //              }
+       //         }
+      //      } 
+     //        stage('Sonar Check') {
+     //           steps {
+       //             script {  
+      //                    env.ARGS="-Dsonar.sources=."
+      //                   common.sonarCheck()
                         
                         
-                    }
-                }
-            } 
-            stage('Test Cases') {
-            parallel {
-                stage('Unit Tests') {
-                     steps {
-                         sh 'echo Unit Test Cases Completed'
-                          }
-                     }
-                 stage('Integration Tests') {
-                     steps {
-                         sh 'echo Integration Test Cases Completed'
-                          }
-                     }
-                 stage('Functional Tests') {
-                     steps {
-                         sh 'echo Functional Test Cases Completed'
-                          }
-                     }
-                 }
-            }    // end of statges 
-         }
-     }
- }    // end of statges 
+       //             }
+       //         }
+       //     } 
+       //     stage('Test Cases') {
+       //     parallel {
+       //         stage('Unit Tests') {
+       //              steps {
+      //                   sh 'echo Unit Test Cases Completed'
+      //                    }
+       //              }
+       //          stage('Integration Tests') {
+       //              steps {
+       //                  sh 'echo Integration Test Cases Completed'
+      //                    }
+     //                }
+   //              stage('Functional Tests') {
+  //                   steps {
+  //                       sh 'echo Functional Test Cases Completed'
+  //                        }
+  //                   }
+ //                }
+  //          }    // end of statges 
+ //        }
+ //    }
+ //}    // end of statges 
     
